f8eba6da919313d867788b54a91c4d431be1bb7a
kalium <kalium@kalium.xyz>
2020-03-10 15:59:42 +0900

    feat(stack) parse michelson in preview window
    for stack effect visualization we need a subset of the michelson grammar (the
    datatype description syntax) to have highlighting

--- apps/michelson-editor/src/app/editor-preview/editor-preview.component.ts
+++ apps/michelson-editor/src/app/editor-preview/editor-preview.component.ts
@@ -1,15 +1,42 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core"
+import { ChangeDetectionStrategy, Component} from "@angular/core"
+import { Connect, Effect, Effects, State } from "ng-effects"
+import { DomSanitizer } from '@angular/platform-browser';
+import { MICHELSON_TOKENS_PROVIDER } from "@coinless/vs-components"
+import * as Monaco from "monaco-editor"
+import { editor } from "monaco-editor"
+import { from } from "rxjs"
+import { map } from 'rxjs/operators'
 
 @Component({
     selector: "bde-editor-preview",
     template: `
-        <!--        <img src="preview/logo.png" alt="" />-->
-    `,
+<div [innerHTML]="content" > </div>
+`,
     styleUrls: ["./editor-preview.component.css"],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [Effects],
 })
 export class EditorPreviewComponent implements OnInit {
-    constructor() {}
+    content: String;
+    sanitizer: DomSanitizer;
 
-    ngOnInit(): void {}
+    constructor(connect: Connect, sanitizer: DomSanitizer) {
+        monaco.languages.register({
+            id: "michelson",
+            aliases: ["tz"],
+        })
+
+        monaco.languages.setMonarchTokensProvider("michelson", MICHELSON_TOKENS_PROVIDER)
+
+        this.sanitizer = sanitizer
+        this.content = `(Pair Unit (Pair Unit string))`
+
+        connect(this)
+    }
+
+    @Effect("content")
+    setContent(state: State<EditorPreviewComponent>) {
+        return from(editor.colorize(this.content, "michelson", {tabsize: 4}))
+            .pipe(map(this.sanitizer.bypassSecurityTrustHtml))
+    }
 }
